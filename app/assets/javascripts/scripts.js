7f46241742b2f972cd4a54f2310e2666bb9a8328
Gabe <gabedominguez@outlook.com>
2013-07-10 18:03:40 -0700

    added js for easier navigation between pages

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-  $('.slidesjs-slide').on("click", function(event){
-  alert('foo');
+  var doc = $(document);
+  var pic = $('.slidesjs-slide');
+  var next = $('.slidesjs-next');
+  var previous = $('.slidesjs-previous');
+
+  pic.on("click", function(event){
+  next.trigger('click');
   });
+
+  doc.on('keyup', function(event) {
+        if (event.keyCode == 39) {
+          next.trigger('click');
+        }
+      });
+
+  doc.on('keyup', function(event) {
+        if (event.keyCode == 37) {
+          previous.trigger('click');
+        }
+      });
 });
